c3702ba3d7d43492acf84ecb637fc8734174eed8
vadiveld <vadi88@gmail.com>
2024-11-03 21:56:24 +0100

    data exploration

--- data_exploration.R
+++ data_exploration.R
@@ -1,3 +1,4 @@
+## Library ==== 
 library(tidyverse)
 library(gapminder)
 library(ggthemes)
@@ -56,7 +57,7 @@ gapminder_2007_tib |>
     ) 
 
 
-### DATA Viz  ---- 
+## DATA Viz  ---- 
 
 
 plot_gbp_life <- gapminder_2007_tib |> 
@@ -94,7 +95,7 @@ plot_gbp_life +
 
 
 
-### DATA Wrangling  ---- 
+## DATA Wrangling  ---- 
 percentage_of_users <- read_csv('data/share-of-individuals-using-the-internet.csv')
 
 
@@ -140,6 +141,139 @@ cars_tibble |>
     geom_point()+
     theme_hc()+ scale_colour_hc()
 
+#### Grouping ---- 
+
+summary_evolution <- gapminder_tib |> 
+    group_by(continent,year) |> 
+    summarize(
+        mean_life = mean(lifeExp),
+        median_life = median(lifeExp),
+        lowerquantitle  = quantile(lifeExp,0.25),
+        upperquantitle  = quantile(lifeExp,0.75),
+        n = n(),
+        .groups = 'drop'
+        )
+
+summary_evolution
+
+# Slice min value 
+
+gapminder_2007_tib |> 
+    group_by(continent) |> 
+    slice_min(lifeExp)
+
+summary_evolution |> 
+    mutate(newcol= mean(lowerquantitle)) |> 
+    slice(1)
+
+
+### 
+### Flight dataset 
+library(nycflights13)
+
+
+flight_selected <- flights |> 
+    select(year:dep_time,origin,dest)
+origin_airport_name  <- airports |> 
+    select(faa,name) |> 
+    rename(origin = faa,origin_airport = name)
+
+destination_airport_name  <- airports |> 
+    select(faa,name) |> 
+    rename(dest = faa,destination_airport = name)
+
+
+flight_join <- flight_selected |> 
+    left_join(origin_airport_name,by = 'origin') |> 
+    left_join(destination_airport_name,by='dest') |> 
+    select( -c(origin,dest))
+
+flight_join
+
+#### creating flight number 
+
+flight_number <- seq_along(flight_join$year)
+
+
+flight_join |> 
+    bind_cols(flight_number = flight_number)
+
+
+###
+delays <- flights |> 
+    select(year:dep_delay)
+
+
+delays |> 
+    mutate(across(.fns = is.na)) |> 
+    summarise(across(.fns = sum))
+
+## Considering only flights that departed. 
+
+delays_departed <- delays |> 
+    filter(!is.na(dep_time))
+
+
+delays_departed |> 
+    mutate(across(.fns = is.na)) |> 
+    summarise(across(.fns = sum))
 
 
 
+summary_delay <- delays_departed |> 
+    group_by(year,month,day) |> 
+    summarize(
+        min_delay = min(dep_delay),
+        max_delay = max(dep_delay),
+        mean_delay = mean(dep_delay),
+        median_delay = median(dep_delay),
+        lowerquantitle  = quantile(dep_delay,0.25),
+        upperquantitle  = quantile(dep_delay,0.75),
+        n = n(),
+        .groups = 'drop'
+    ) |> 
+    pivot_longer(
+        cols = -(year:day),
+        values_to = 'value',
+        names_to = 'summary'
+        
+    ) |> 
+    mutate(date = lubridate::make_date(year,month,day))
+
+
+summary_level <- c(
+    'min_delay',
+    'lowerquantitle',
+    'median_delay',
+    'mean_delay',
+    'upperquantitle',
+    'max_delay'
+)
+
+
+summary_delay |> 
+    filter(summary !='n') |> 
+    mutate(summary = factor(summary,levels = summary_level)) |> 
+    ggplot(aes(x = date, y = value , colour = summary))+
+    geom_line(show.legend = F,alpha = 0.5)+
+    geom_smooth(show.legend = F,se = F,span=0.1,size = 1) + 
+    facet_wrap(vars(summary),scale='free_y') + 
+    scale_x_date(date_labels = '%m')+ 
+    labs(
+        x = 'Month',
+        y = 'Delay(in Min)'
+    )+
+    theme_bw()
+
+
+summary_delay |> 
+    filter(! summary %in% c('n','max_delay','min_delay')) |> 
+    mutate(summary = factor(summary,levels = summary_level)) |> 
+    ggplot(aes(x = date, y = value , colour = summary))+
+    geom_smooth(,span=0.1) + 
+    scale_x_date(date_labels = '%m',date_breaks = '1 month')+ 
+    labs(
+        x = 'Month',
+        y = 'Delay(in Min)'
+    )+
+    theme_bw()
